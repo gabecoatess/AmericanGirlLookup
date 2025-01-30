c70e6a9d1cd1bfe7fdeff359497a9a322ca38c9e
Gabe <54961758+gabecoatess@users.noreply.github.com>
2025-01-30 11:34:47 -0500

    Add basic Doll Lookup feature with supporting controller and views
    - Added a navigation link for "Doll Lookup" in `_Layout.cshtml`
    - Created `DollLookup` controller with `Index` and `Doll` actions
    - Added `Doll.cshtml` view to display doll name
    - Added `Index.cshtml` view with title and welcome message for Doll Lookup

---
+++ AmericanGirlLookup/Controllers/DollLookup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AmericanGirlLookup.Controllers
+{
+    public class DollLookup : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Doll(string dollName)
+        {
+            ViewData["DollName"] = dollName;
+
+            return View();
+        }
+    }
+}
