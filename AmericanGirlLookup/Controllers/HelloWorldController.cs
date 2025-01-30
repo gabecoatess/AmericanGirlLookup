608812bfc789823125b233fe162110affcd17e43
Gabe <54961758+gabecoatess@users.noreply.github.com>
2025-01-30 10:43:29 -0500

    Added HelloWorld controller and view
    - Added a new navigation link to the "Hello World" page in `_Layout.cshtml`.
    - Created `HelloWorldController` with `Index` and `Welcome` actions.
    - Added `Index.cshtml` view for the `Index` action of `HelloWorldController`.

---
+++ AmericanGirlLookup/Controllers/HelloWorldController.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AmericanGirlLookup.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome(string name, int age)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}! You are {age} years old!");
+        }
+    }
+}
